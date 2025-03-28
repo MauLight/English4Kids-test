033a5bff7506f7e32e23bc06a7bd6571bafe9d2a
Mau Ulisse Luz <maulisseluz@gmail.com>
2025-03-28 20:24:33 -0300

    feat: added design system to vite project
    font, sizes nd color pallette

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,14 +1,10 @@
-import { animatedGradientText } from "../utils/styles"
-import Symetria from '../assets/imgs/Symetria.svg'
-
 
 function Home() {
     return (
-        <div className="relative w-full h-screen flex justify-center items-center bg-black">
-            <img className="w-screen h-screen absolute top-0 z-10" src={Symetria} />
-            <div className="flex flex-col justify-center items-center z-20">
-                <h1 className={`font-title ${animatedGradientText} text-[3rem] uppercase`}>Hello Symetria</h1>
-                <p className="text-[1rem] text-gray-50">This is Symetria Boilerplate.</p>
+        <div className="relative w-full h-screen flex justify-center items-center bg-primary px-5">
+            <div className="flex flex-col justify-center w-full max-w-[420px] h-[320px] rounded-[25px] bg-dark items-center z-20">
+                <h1 className={`font-title text-text text-header uppercase`}>Hello World</h1>
+                <p className="text-regular text-text2">This is a longer sentence.</p>
             </div>
         </div>
     )
